4ee0e126789b71207a170beaa2fe3dc50771b1ce
Ramil <romashkin.ramil@ya.ru>
2024-11-11 16:41:52 +0300

    start

--- main.py
+++ main.py
@@ -3,38 +3,22 @@ from pydantic import BaseModel, ConfigDict
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from database import create_tables, delete_tables
+from schemas import STaskAdd
+from router import router as tasks_router
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    await create_tables()
-    print("База готова")
-    yield
     await delete_tables()
     print("База очищена")
+    await create_tables()
+    print("База готова к работе")
+    yield
+    print("Выключение")
 
 
 app = FastAPI(lifespan=lifespan)
-
-
-class STaskAdd(BaseModel):
-    name: str
-    description: str | None = None
-
-
-class STask(STaskAdd):
-    id: int
-    model_config = ConfigDict(from_attributes=True)
-
-
-@app.get("/")
-async def home():
-    return {"data": "Hello World"}
-
-
-@app.post("/")
-async def add_task(task: STaskAdd = Depends()):
-    return {"data": task}
+app.include_router(tasks_router)
 
 
 # uvicorn main:app --reload
